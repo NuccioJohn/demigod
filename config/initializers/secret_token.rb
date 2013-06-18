47956621b72e3641d39a8f85c83b721a58fec94c
John Nuccio <nuccio.john@gmail.com>
2013-06-18 16:37:06 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demigod::Application.config.secret_token = '4b89fa0c71cb7596e7a5dedb374511e6d3054bd9197bd618ddf60bd83d8c55c6e48f3713340dd7d9b704e403d05c130e14551b88b56864dcbb773534cbc5c69d'
